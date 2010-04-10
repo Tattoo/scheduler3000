7e6a4793828161ee72a597cbeef422bfac669b5d
tatu.kairi@gmail.com <tatu.kairi@gmail.com@86293e66-bf6e-8e1e-1f5c-319a1f79e9fa>
2010-04-10 18:29:14 +0000

    source for proto

---
+++ src/FullReport.java
@@ -0,0 +1,16 @@
+package ohtuk10;
+
+/**
+ * @author Team TA's
+ */
+
+import java.util.HashMap;
+
+
+public class FullReport extends Report {
+
+	public FullReport(Schedule schedule, HashMap<String, Object> options) {
+		super(schedule, options);
+	}
+
+}
